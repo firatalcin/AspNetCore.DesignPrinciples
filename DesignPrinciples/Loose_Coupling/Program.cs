6acbf8642945078b953e04cfd59dccaf06ee9c02
Fırat Alçın <firatalcin04@gmail.com>
2024-03-13 01:10:14 +0300

    Loose Coupling

---
+++ DesignPrinciples/Loose_Coupling/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Loose_Coupling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MailSender mailSender = new MailSender();
+            mailSender.Send(new Outlook());
+        }
+    }
+
+    class MailSender
+    {
+        public void Send(IMailServer mailServer)
+        {
+            mailServer.Send("firatalcin@gmail.com", "Onemli");
+        }
+    }
+
+    interface IMailServer
+    {
+        void Send(string to, string body);
+    }
+
+
+    class Gmail : IMailServer
+    {
+        public void Send(string to, string body)
+        {
+            Console.WriteLine("Gmail Maili Atıldı");
+        }
+    }
+
+    class Outlook : IMailServer
+    { 
+        public void Send(string to, string body)
+        {
+            Console.WriteLine("Outlook Maili Atıldı");
+        }
+    }
+
+}
