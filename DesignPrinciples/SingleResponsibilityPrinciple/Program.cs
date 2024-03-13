2585c9c7bbe5f3174b1ab4280aa7551b0d1bc1ad
Fırat Alçın <firatalcin04@gmail.com>
2024-03-13 18:15:47 +0300

    Single Responsibility

---
+++ DesignPrinciples/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SingleResponsibilityPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            //person.GetPersonList();
+
+            PersonManager personManager = new PersonManager();
+            personManager.GetPersonList();
+        }
+    }
+    // Entity'ler sadece property tutarlar
+    class Person
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //public void GetPersonList()
+        //{
+        //    Console.WriteLine("Person List");
+        //}
+    }
+
+    class PersonManager
+    {
+        public void GetPersonList()
+        {
+            Console.WriteLine("Person List");
+        }
+    }
+}
