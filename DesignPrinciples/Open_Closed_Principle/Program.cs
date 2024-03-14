eb0cafefd61bb924151abc57eb6af757348e8748
Fırat Alçın <firatalcin04@gmail.com>
2024-03-14 22:55:00 +0300

    Open/Closed

---
+++ DesignPrinciples/Open_Closed_Principle/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Open_Closed_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gonder(new Halkbank(), 1000, "123456");
+        }
+
+        static void Gonder(IBanka banka, int tutar, string hesapNo)
+        {
+            banka.ParaTransferi(tutar, hesapNo);
+        }
+    }
+
+    interface IBanka
+    {
+        bool ParaTransferi(int tutar, string hesapNo);
+    }
+
+    class Garanti : IBanka
+    {
+        public string HesapNo { get; set; }
+        public void ParaGonder(int tutar)
+        {
+            //...
+        }
+
+        public bool ParaTransferi(int tutar, string hesapNo)
+        {
+            try
+            {
+
+                HesapNo = hesapNo;
+                ParaGonder(tutar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    class Halkbank : IBanka 
+    {
+        string _hesapNo;
+        public void GonderilecekHesapNo(string hesapNo)
+        {
+            //...
+        }
+        public void Gonder(int tutar)
+        {
+            //...
+        }
+
+        public bool ParaTransferi(int tutar, string hesapNo)
+        {
+            try
+            {
+                GonderilecekHesapNo(hesapNo);
+                Gonder(tutar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
