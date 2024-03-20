bd53eee100ac94c2f3f2d98cd6c99f6517ed9329
Fırat Alçın <firatalcin04@gmail.com>
2024-03-20 22:03:39 +0300

    Dependency Inversion Principle

---
+++ DesignPrinciples/Dependency_Inversion_Principle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Dependency_Inversion_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MailService mailService = new MailService();
+            mailService.SendMail(new GMail(), "...", "...");
+        }
+    }
+
+
+    class MailService
+    {
+        public void SendMail(IMailService mailService, string to, string body)
+        {
+            mailService.Send(to, body);
+        }
+    }
+
+    interface IMailService
+    {
+        void Send(string to, string body);
+    }
+
+    class GMail : IMailService
+    {
+        public void Send(string to, string body)
+        {
+            Console.WriteLine("GMail ile gönderildi");
+        }
+    }
+
+    class Outlook : IMailService
+    {
+        public void Send(string to, string body)
+        {
+            Console.WriteLine("Outlook ile gönderildi");
+        }
+    }
+
+    class Yandex : IMailService
+    {
+        public void Send(string to, string body)
+        {
+            Console.WriteLine("Yandex ile gönderildi");
+        }
+    }
+}
